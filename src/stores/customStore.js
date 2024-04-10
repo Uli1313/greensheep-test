a5b4278deed6501e84aa5a8058316eab578a656e
Uli <ru21041555@gmail.com>
2024-04-10 23:29:49 +0800

    fix - image file name

--- src/stores/customStore.js
+++ src/stores/customStore.js
@@ -6,73 +6,73 @@ export default defineStore('customStore', {
         gemMonth: '1月',
         gemChName: '石榴石',
         genEnName: 'Garnet',
-        gemUrl: '/greensheep-test/src/assets/images/1month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month1.png'
       },
       {
         gemMonth: '2月',
         gemChName: '紫水晶',
         genEnName: 'Amethyst',
-        gemUrl: '/greensheep-test/src/assets/images/2month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month2.png'
       },
       {
         gemMonth: '3月',
         gemChName: '海藍寶',
         genEnName: 'Aquamarine',
-        gemUrl: '/greensheep-test/src/assets/images/3month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month3.png'
       },
       {
         gemMonth: '4月',
         gemChName: '鑽石',
         genEnName: 'Diamond',
-        gemUrl: '/greensheep-test/src/assets/images/4month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month4.png'
       },
       {
         gemMonth: '5月',
         gemChName: '祖母綠',
         genEnName: 'Emerald',
-        gemUrl: '/greensheep-test/src/assets/images/5month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month5.png'
       },
       {
         gemMonth: '6月',
         gemChName: '月光石',
         genEnName: 'Moonstone',
-        gemUrl: '/greensheep-test/src/assets/images/6month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month6.png'
       },
       {
         gemMonth: '7月',
         gemChName: '紅寶石',
         genEnName: 'Ruby',
-        gemUrl: '/greensheep-test/src/assets/images/7month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month7.png'
       },
       {
         gemMonth: '8月',
         gemChName: '橄欖石',
         genEnName: 'Peridot',
-        gemUrl: '/greensheep-test/src/assets/images/8month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month8.png'
       },
       {
         gemMonth: '9月',
         gemChName: '藍寶石',
         genEnName: 'Sapphire',
-        gemUrl: '/greensheep-test/src/assets/images/9month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month9.png'
       },
       {
         gemMonth: '10月',
         gemChName: '粉碧璽',
         genEnName: 'Tourmaline',
-        gemUrl: '/greensheep-test/src/assets/images/10month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month10.png'
       },
       {
         gemMonth: '11月',
         gemChName: '黃水晶',
         genEnName: 'Citrine',
-        gemUrl: '/greensheep-test/src/assets/images/11month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month11.png'
       },
       {
         gemMonth: '12月',
         gemChName: '坦桑石',
         genEnName: 'Tanzanite',
-        gemUrl: '/greensheep-test/src/assets/images/12month.png'
+        gemUrl: '/greensheep-test/src/assets/images/month12.png'
       }
     ]
   })
